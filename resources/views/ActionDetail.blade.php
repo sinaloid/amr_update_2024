c464e741ca7c194398ceefcb776b4e705733444e
Sinali Dionou <sinaloid@gmail.com>
2024-09-10 17:39:46 +0000

    update success story

--- resources/views/ActionDetail.blade.php
+++ resources/views/ActionDetail.blade.php
@@ -3,7 +3,7 @@
     <!-- Page Header Start -->
     <div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="container py-5">
-            <h1 class="display-3 text-white mb-3 animated slideInDown">Actualités</h1>
+            <h1 class="display-3 text-white mb-3 animated slideInDown">Actions & Stories</h1>
         </div>
     </div>
     <!-- Page Header End -->
